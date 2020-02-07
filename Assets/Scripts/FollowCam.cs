c835d599ed16a60ae84e030dddc2ddecf2691bf1
Justin <jkoh8@uwo.ca>
2020-02-07 00:37:31 -0500

    rigged and animated huntress model added blend file, some prototyping assets, added player controller and 3rd person camera

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    [SerializeField] float mouse_sensitivity;
+    [SerializeField] Transform target;
+    [SerializeField] float distance_from_target;
+    [SerializeField] Vector2 pitch_min_max;
+
+    [SerializeField] float rotation_smooth_time;
+
+    private Vector3 rotation_smooth_velocity;
+    private Vector3 curr_rotation;
+
+    private float yaw;
+    private float pitch;
+
+    private void LateUpdate()
+    {
+        yaw += Input.GetAxis("Mouse X") * mouse_sensitivity;
+        pitch -= Input.GetAxis("Mouse Y") * mouse_sensitivity;
+        pitch = Mathf.Clamp(pitch, pitch_min_max.x, pitch_min_max.y);
+
+        curr_rotation = Vector3.SmoothDamp(curr_rotation, new Vector3(pitch, yaw), ref rotation_smooth_velocity, rotation_smooth_time);
+
+        Vector3 target_rotation = new Vector3(pitch, yaw);
+        this.transform.eulerAngles = target_rotation;
+
+        this.transform.position = target.position - transform.forward * distance_from_target;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float walk_speed;
+    [SerializeField] float run_speed;
+    [SerializeField] float turn_smooth_time;
+
+    private float turn_smooth_velocity;
+
+    [SerializeField] float speed_smooth_time;
+    private float speed_smooth_velocity;
+    private float curr_speed;
+
+    private float speed;
+    private float animation_speed_percent;
+    private Animator animator;
+
+    private Transform camera_transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+        camera_transform = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 input_direction = input.normalized;
+        if (input_direction != Vector2.zero)
+        {
+            float target_rotation = Mathf.Atan2(input_direction.x, input_direction.y) * Mathf.Rad2Deg + camera_transform.eulerAngles.y;
+            this.transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(this.transform.eulerAngles.y, target_rotation, ref turn_smooth_velocity, turn_smooth_time);
+        }
+
+        bool is_running = Input.GetKey(KeyCode.LeftShift);
+
+        float target_speed = (is_running ? run_speed : walk_speed) * input_direction.magnitude;
+        curr_speed = Mathf.SmoothDamp(curr_speed, target_speed, ref speed_smooth_velocity, speed_smooth_time);
+
+        this.transform.Translate(this.transform.forward * curr_speed * Time.deltaTime, Space.World);
+        
+        animation_speed_percent = (is_running ? 1f : 0.5f) * input_direction.magnitude;
+        animator.SetFloat("speedPercent", animation_speed_percent, speed_smooth_time, Time.deltaTime);
+    }
+}
