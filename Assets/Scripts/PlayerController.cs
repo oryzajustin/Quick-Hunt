7b266161bccb70c1cce451761424c0f423449f31
Justin <jkoh8@uwo.ca>
2020-02-07 19:24:23 -0500

    shader pack, fixed foot IK placements, other tweaks

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,34 +18,53 @@ public class PlayerController : MonoBehaviour
     private float animation_speed_percent;
     private Animator animator;
 
+    private float gravity = -12f;
+    private float velocity_y;
+
     private Transform camera_transform;
+
+    private CharacterController controller;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = this.GetComponent<Animator>();
         camera_transform = Camera.main.transform;
+        controller = this.GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        Vector2 input_direction = input.normalized;
-        if (input_direction != Vector2.zero)
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")); // controller input
+        Vector2 input_direction = input.normalized; // normalize the direction vector to prevent fast diagonal movement
+        if (input_direction != Vector2.zero) // rotate the player model towards the input direction
         {
             float target_rotation = Mathf.Atan2(input_direction.x, input_direction.y) * Mathf.Rad2Deg + camera_transform.eulerAngles.y;
             this.transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(this.transform.eulerAngles.y, target_rotation, ref turn_smooth_velocity, turn_smooth_time);
         }
 
-        bool is_running = Input.GetKey(KeyCode.LeftShift);
+        bool is_running = Input.GetKey(KeyCode.LeftShift); // check if running
 
-        float target_speed = (is_running ? run_speed : walk_speed) * input_direction.magnitude;
-        curr_speed = Mathf.SmoothDamp(curr_speed, target_speed, ref speed_smooth_velocity, speed_smooth_time);
-
-        this.transform.Translate(this.transform.forward * curr_speed * Time.deltaTime, Space.World);
+        float target_speed = (is_running ? run_speed : walk_speed) * input_direction.magnitude; // the speed we want to reach
+        curr_speed = Mathf.SmoothDamp(curr_speed, target_speed, ref speed_smooth_velocity, speed_smooth_time); // damp to the target speed from our current speed
         
-        animation_speed_percent = (is_running ? 1f : 0.5f) * input_direction.magnitude;
-        animator.SetFloat("speedPercent", animation_speed_percent, speed_smooth_time, Time.deltaTime);
+        velocity_y += gravity * Time.deltaTime;
+        Vector3 velocity = this.transform.forward * curr_speed + Vector3.up * velocity_y; // velocity
+
+        if (controller.isGrounded) 
+        {
+            velocity_y = 0;
+        }
+
+        controller.Move(velocity * Time.deltaTime);
+
+        animation_speed_percent = (is_running ? 1f : 0.5f) * input_direction.magnitude; // handles the animation speed percent
+        animator.SetFloat("speedPercent", animation_speed_percent, speed_smooth_time, Time.deltaTime); // dampen the animation to the target animation
     }
 }
